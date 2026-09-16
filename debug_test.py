04d8c85b8a13e1e1287eb10ad9d6171894f8a8b6
Paulo125k <paulofelippe2008@gmail.com>
2026-09-16 16:15:59 -0300

    chore: remove script de debug

--- debug_test.py
+++
@@ -1,9 +0,0 @@
-from app.integrations.igdb_client import get_access_token, search_game
-
-token = get_access_token()
-jogo = search_game("Skyrim", token)
-
-if jogo is None:
-    print("Jogo não encontrado.")
-else:
-    print(jogo)
